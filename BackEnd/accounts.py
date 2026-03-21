eef44b5be3ec634984c95aa287de2b96abf70e73
dylanh219 <dmholmes02@gmailcom>
2026-03-21 15:37:48 -0500

    Created login/sign up script, will test more later

---
+++ BackEnd/accounts.py
@@ -0,0 +1,49 @@
+from hashlib import sha256 as encrypt
+import database as db
+import psycopg2
+
+def register(email, password):
+    encrypted_password = encrypt(password.encode()).hexdigest()
+    if not check_email_exists(email):
+        save_user_to_database(email, encrypted_password)
+        return "Registration successful"
+    else:
+        return "E-Mail already exists, please login instead"
+
+def check_email_exists(email):
+    for user in get_users_from_database():
+        if user['email'] == email:
+            return True
+        
+    return False
+
+def save_user_to_database(email, encrypted_password):
+    conn = psycopg2.connect(db.databaseURL)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (email, password) VALUES (%s, %s)", (email, encrypted_password))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def login(username, password):
+    encrypted_password = encrypt(password.encode()).hexdigest()
+    if check_credentials(username, encrypted_password):
+        return "Login successful"
+    else:
+        return "Invalid username or password" 
+
+def check_credentials(username, encrypted_password):
+    for user in get_users_from_database():
+        if user['username'] == username and user['password'] == encrypted_password:
+            return True
+        
+    return False
+
+def get_users_from_database():
+    conn = psycopg2.connect(db.databaseURL)
+    cursor = conn.cursor()
+    cursor.execute("SELECT email, password FROM users")
+    users = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return [{'email': user[0], 'password': user[1]} for user in users]
---
+++ BackEnd/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+databaseURL = "postgresql://neondb_owner:npg_Eeiwx1QKqZT9@ep-falling-cherry-amwmudme-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+
+
+engine = create_engine(databaseURL)
+with engine.connect() as conn:
+    result = conn.execute(text())
+
+    # Create SessionLocal class
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    # Create a Base class for models
+    Base = declarative_base()
+
+    # Dependency to get DB session
+    def get_db():
+        db = SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
