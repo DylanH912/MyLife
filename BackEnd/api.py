7e8e4f64a4f6095aa8afb603082cf2395d9e07bd
dylanh219 <dmholmes02@gmailcom>
2026-03-26 12:47:02 -0500

    analyze_receipt image works now

--- BackEnd/api.py
+++ BackEnd/api.py
@@ -5,8 +5,7 @@ from fastapi import FastAPI, Depends, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel # FIXED: Added for JSON body support
 from accounts import register, login
-import httpx
-import io
+import time
 import os
 from dotenv import load_dotenv, dotenv_values 
 load_dotenv()
@@ -60,13 +59,22 @@ async def analyze_food_image(file: UploadFile = File(...)): # FIXED: Now accepts
 
 @app.post("/receipt")
 async def analyze_receipt_image(file: UploadFile = File(...), mode: str = Form(...), extraflag = False): # FIXED: Now accepts a real file upload
-    url = "https://api.tabscanner.com/api/2/process"  # FIXED: Tabscanner API endpoint
+    url = "https://api.tabscanner.com/api/2/process/"  # FIXED: Tabscanner API endpoint
+    result_url = "https://api.tabscanner.com/api/result/{0}"
     print(os.getenv("TABSCANNER_API_KEY"))
-    headers = {"X-API-Key": os.getenv("TABSCANNER_API_KEY")}
-    
+    headers = {"apikey": os.getenv("TABSCANNER_API_KEY")}
+    payload = {"documentType":"receipt"}    
     content = await file.read()
     files = {"file": (file.filename, content, file.content_type)}
     
-    response = httpx.post(url, headers=headers, files=files)
-    response.raise_for_status()
-    return response.json()
+    response = requests.post( url,
+                              files=files,
+                              data=payload,
+                              headers=headers)
+    token = response.json().get("token")
+    endpoint = result_url.format(token)
+    time.sleep(5)  # FIXED: Wait for processing
+    result = requests.get(endpoint, headers=headers)
+    result.raise_for_status()
+    print(result.text) # DEBUG: Print the raw response from Tabscanner
+    return result.text
--- BackEnd/testapi.py
+++ BackEnd/testapi.py
@@ -1,6 +1,9 @@
 from fastapi import testclient
 from api import app
+import PIL.Image as Image
 
+img = Image.open("C:/MyLife/BackEnd/R.jpg")
+img.resize((720, 1280)).save("C:/MyLife/BackEnd/R.jpg") # Resize to 224x224 for testing
 client = testclient.TestClient(app)
 
 def test_analyze_receipt_image():
@@ -11,7 +14,6 @@ def test_analyze_receipt_image():
             data={"mode": "your_mode"} 
         )
     assert response.status_code == 200
-    print(response.json())
 
 
 test_analyze_receipt_image()
